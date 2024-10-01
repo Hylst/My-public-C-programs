c1f205ee31c45351b316bb3f4ad969ecd4017cc2
Hylst <geoffroy.streit@gmail.com>
2024-10-01 16:45:24 +0200

    1st commit non functionnal game

---
+++ TEXT_MOTO_RACER/readme.md
@@ -0,0 +1,42 @@
+# Moto Game: The Ultimate Test of Your (Virtual) Riding Skills or... not... ^^
+
+**Because who needs a real motorcycle when you can ride a virtual one, right?**
+
+## Features
+
+* **Simple and fun ASCII art moto game:**  Because sometimes, less is more.
+* **Avoid obstacles and vehicles:**  You're not a daredevil, are you?
+* **Collect fuel to keep your moto running:**  Don't let your bike sputter out!
+* **Earn points and survive as long as you can:**  How long can you last before you crash and burn?
+
+## Requirements
+
+* **A C compiler (gcc or clang):**  Because you need something to turn code into magic.
+* **A terminal or console:**  Because who wants to play a game on a fancy GUI?
+
+## Compilation and Execution
+
+1. **Clone this repository:** Because you need the code to play the game.
+2. **Compile the code with the command `gcc -o moto_game moto_game.c`:**  Because magic is made with code.
+3. **Run the game with the command `./moto_game`:**  Because now it's time to ride!
+
+## Controls
+
+* **A/D:** Move your moto left or right.
+* **W/S:** Move your moto up or down.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
+
+## Contributors
+
+* **[Hylst the looser]:**  The mastermind behind this epic game.
+
+## Notes
+
+This game is a simple project designed to be a beginner-friendly introduction to game development. It's possible to extend it with additional features, more elaborate graphics, and improved physics.
+
+## Contact
+
+If you have any questions or suggestions, feel free to contact me.
---
+++ TEXT_MOTO_RACER/txt_moto_racer.c
@@ -0,0 +1,293 @@
+// Moto Game: Because who needs a social life when you can ride a virtual motorcycle?
+
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <time.h>
+
+// Constants: because who doesn't love a good constant?
+#define WIDTH 80
+#define HEIGHT 30
+#define SPEED 100000
+
+typedef struct {
+    int x;
+    int y;
+    int type;
+} Entity;
+
+// Structure to hold the moto's position and fuel level
+typedef struct {
+    int x;
+    int y;
+    int fuel;
+    int score;
+} Moto;
+
+#ifdef _WIN32
+void usleep(int microseconds) {
+    Sleep(microseconds / 1000);
+}
+#else
+// usleep() est déjà définie dans <unistd.h>
+#endif
+
+// Function to clear the screen, because we don't want any clutter
+void clear_screen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+// Function to print the moto, because we want to see our beautiful bike
+void print_moto(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+    printf(" /_/\\ ");
+    printf("\033[%d;%dH", y+1, x);
+    printf("(o.o )");
+    printf("\033[%d;%dH", y+2, x);
+    printf(" > ^ <");
+}
+
+// Function to print a car, because cars are the bane of our existence
+void print_car(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+    printf(" ____");
+    printf("\033[%d;%dH", y+1, x);
+    printf("/|__\\\\");
+    printf("\033[%d;%dH", y+2, x);
+    printf("O-O-O");
+}
+
+// Function to print a truck, because trucks are even worse than cars
+void print_truck(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+    printf(" _____");
+    printf("\033[%d;%dH", y+1, x);
+    printf("|     |__");
+    printf("\033[%d;%dH", y+2, x);
+    printf("|_______||");
+    printf("\033[%d;%dH", y+3, x);
+    printf(" O     O ");
+}
+
+// Function to print fuel, because we need to keep our moto running
+void print_fuel(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+    printf(" ___ ");
+    printf("\033[%d;%dH", y+1, x);
+    printf("|_F_|");
+    printf("\033[%d;%dH", y+2, x);
+    printf(" | | ");
+}
+
+// Function to print an obstacle, because obstacles are just trying to ruin our fun
+void print_obstacle(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+    printf(" /\\ ");
+    printf("\033[%d;%dH", y+1, x);
+    printf("/**\\");
+    printf("\033[%d;%dH", y+2, x);
+    printf("\\__/");
+}
+
+// Function to print the road, because we need something to ride on
+void print_road(char road[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            printf("%c", road[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Function to initialize the road, because we need a clean slate
+void init_road(char road[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (j == 0 || j == WIDTH - 1) {
+                road[i][j] = '|';
+            } else if (j == WIDTH / 3 || j == 2 * WIDTH / 3) {
+                road[i][j] = '.';
+            } else {
+                road[i][j] = ' ';
+            }
+        }
+    }
+}
+
+// Function to update the road with the moto and obstacles, because we need to see where we're going
+void update_road(char road[HEIGHT][WIDTH], Moto *moto, Entity entities[], int entity_count) {
+    init_road(road);
+    
+    for (int i = 0; i < entity_count; i++) {
+        switch (entities[i].type) {
+            case CAR:
+                print_car(entities[i].x, entities[i].y);
+                break;
+            case TRUCK:
+                print_truck(entities[i].x, entities[i].y);
+                break;
+            case FUEL:
+                print_fuel(entities[i].x, entities[i].y);
+                break;
+            case OBSTACLE:
+                print_obstacle(entities[i].x, entities[i].y);
+                break;
+        }
+    }
+    
+    print_moto(moto->x, moto->y);
+}
+
+// Function to move the obstacles and vehicles, because they're always on the move
+void move_entities(Entity entities[], int entity_count) {
+    for (int i = 0; i < entity_count; i++) {
+        entities[i].y++;
+        if (entities[i].y >= HEIGHT) {
+            entities[i].y = 0;
+            entities[i].x = rand() % (WIDTH - 10) + 5;
+            
+            int r = rand() % 100;
+            if (r < 60) {
+                entities[i].type = OBSTACLE;
+            } else if (r < 80) {
+                entities[i].type = CAR;
+            } else if (r < 95) {
+                entities[i].type = TRUCK;
+            } else {
+                entities[i].type = FUEL;
+            }
+        }
+    }
+}
+
+// Function to check for collisions, because we don't want to crash
+int check_collision(Moto *moto, Entity entities[], int entity_count) {
+    for (int i = 0; i < entity_count; i++) {
+        if (abs(entities[i].x - moto->x) < 3 && abs(entities[i].y - moto->y) < 3) {
+            if (entities[i].type == FUEL) {
+                moto->fuel += 20;
+                if (moto->fuel > 100) moto->fuel = 100;
+                moto->score += 10;
+                entities[i].y = 0;
+                entities[i].x = rand() % (WIDTH - 10) + 5;
+            } else {
+                return 1; // Collision detected
+            }
+        }
+    }
+    return 0; // No collision
+}
+
+// Function to play a sound, because we want to hear the excitement
+void play_sound() {
+#ifdef _WIN32
+    Beep(2500, 100);
+#else
+    printf("\a");
+#endif
+}
+
+// The main function, where all the magic happens
+int main() {
+    srand(time(NULL));
+
+    // Print the title, because we need to know what we're playing
+    printf("  _______  _______  _______  _______  _______  _______  _______ \n");
+    printf(" |       ||       ||       ||       ||       ||       ||       |\n");
+    printf(" |  _____||_     _||   _   ||   _   ||   _   ||   _   ||   _   |\n");
+    printf(" | |_____   |   |  |  | |  |  | |  |  | |  |  |  | |  |  | |  |\n");
+    printf(" |_____  |  |   |  |  |_|  |  |_|  |  |_|  |  |  |_|  |  |_|  |\n");
+    printf("  _____| |  |   |  |       |       |       |  |       |       |\n");
+    printf(" |_______|  |___|  |_______|_______|_______|  |_______|_______|\n");
+    printf("  _______  _______  _______  _______  _______  _______  _______ \n");
+    printf(" |       ||       ||       ||       ||       ||       ||       |\n");
+    printf(" |  _____||_     _||   _   ||   _   ||   _   ||   _   ||   _   |\n");
+    printf(" | |_____   |   |  |  | |  |  | |  |  | |  |  |  | |  |  | |  |\n");
+    printf(" |_____  |  |   |  |  |_|  |  |_|  |  |_|  |  |  |_|  |  |_|  |\n");
+    printf("  _____| |  |   |  |       |       |       |  |       |       |\n");
+    printf(" |_______|  |___|  |_______|_______|_______|  |_______|_______|\n");
+    printf("\n");
+    printf("  Welcome to Moto Game! \n");
+    printf("  Use the A (left), D (right), W (up), S (down) keys to move. \n");
+    printf("  Avoid obstacles and vehicles, collect fuel. \n");
+    printf("  Press any key to start...\n");
+    getchar();
+
+    // Initialize the moto
+    Moto moto = {WIDTH / 2, HEIGHT - 5, 100, 0};
+    char road[HEIGHT][WIDTH];
+    
+    // Initialize the obstacles and vehicles
+    Entity entities[20];
+    for (int i = 0; i < 20; i++) {
+        entities[i].x = rand() % (WIDTH - 10) + 5;
+        entities[i].y = rand() % HEIGHT;
+        int r = rand() % 100;
+        if (r < 60) {
+            entities[i].type = OBSTACLE;
+        } else if (r < 80) {
+            entities[i].type = CAR;
+        } else if (r < 95) {
+            entities[i].type = TRUCK;
+        } else {
+            entities[i].type = FUEL;
+        }
+    }
+
+    // The main game loop, where the action happens
+    while (1) {
+        clear_screen();
+        update_road(road, &moto, entities, 20);
+        print_road(road);
+        
+        printf("Fuel: %d%% | Score: %d\n", moto.fuel, moto.score);
+
+        move_entities(entities, 20);
+
+        if (check_collision(&moto, entities, 20)) {
+            play_sound();
+            printf("Game Over! Final Score: %d\n", moto.score);
+            return 0;
+        }
+
+        moto.fuel--;
+        if (moto.fuel <= 0) {
+            play_sound();
+            printf("Out of fuel! Game Over! Final Score: %d\n", moto.score);
+            return 0;
+        }
+
+        moto.score++;
+
+        usleep(SPEED);
+
+        // Get the player's input
+        char input;
+        if (scanf("%c", &input) == 1) {
+            switch (input) {
+                case 'a':
+                    if (moto.x > 5) moto.x--;
+                    break;
+                case 'd':
+                    if (moto.x < WIDTH - 5) moto.x++;
+                    break;
+                case 'w':
+                    if (moto.y > 0) moto.y--;
+                    break;
+                case 's':
+                    if (moto.y < HEIGHT - 3) moto.y++;
+                    break;
+            }
+        }
+    }
+
+    return 0;
+}
