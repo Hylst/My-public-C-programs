cde96b8ab63fb91739520b3a18075843e74acaf0
Hylst <geoffroy.streit@gmail.com>
2024-10-01 13:43:15 +0200

    +ghost smart moves +snd +crossplatform + highscore

--- PAC_ASCII/pac_ascii.c
+++ PAC_ASCII/pac_ascii.c
@@ -1,134 +1,241 @@
 // PAC ASCII - MINI PAC MAN LIKE GAME IN TEXT MODE
-// v0.2
+// v0.3
 // by Hylst (1994 - 2024 (cleaning code / good practice / comments) )
 // To Do :
-// Change to a cross-platform lib to access terminal functionality. 
-// Implement Ghost moves, boost to eat, ... Everything...
+// add readme.md
+// Complete code
+// Finish implementing of Ghost moves, boost to eat, ... Everything...
+// Add introduction / welcome screen explaining game & controls
+// ...
+
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
+#ifdef _WIN32
+    #include <windows.h> // Pour Sleep et Beep sous Windows
+#else
+    #include <unistd.h>  // Pour usleep sous Unix
+#endif
 
 #define WIDTH 20
 #define HEIGHT 10
-#define EMPTY ' '
+#define PACMAN '@'
+#define GHOST 'G'
 #define WALL '#'
-#define PLAYER1 'P'
-#define PLAYER2 'G'
-#define FOOD '.'
+#define PELLET '.'
 
+// Structure pour position
 typedef struct {
     int x, y;
 } Position;
 
-char board[HEIGHT][WIDTH];
-Position player1, player2;
-int score1 = 0, score2 = 0;
-int foodCount = 0;
-
-void initBoard() {
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            if (x == 0 || x == WIDTH - 1 || y == 0 || y == HEIGHT - 1 || (x % 4 == 0 && y % 2 == 0)) {
-                board[y][x] = WALL; // Obstacles
-            } else {
-                board[y][x] = FOOD;
-                foodCount++;
-            }
-        }
-    }
-
-    // Initialize players
-    player1.x = 1; player1.y = 1;
-    player2.x = WIDTH - 2; player2.y = HEIGHT - 2;
-    board[player1.y][player1.x] = PLAYER1;
-    board[player2.y][player2.x] = PLAYER2;
+// Fonction multiplateforme pour effacer l'écran
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
 
+// Fonction multiplateforme pour gérer les délais
+void wait(int milliseconds) {
+    #ifdef _WIN32
+        Sleep(milliseconds);
+    #else
+        usleep(milliseconds * 1000); // Conversion en microsecondes pour Unix
+    #endif
+}
+
+// Variables globales pour Pac-Man et les fantômes
+Position pacman = {1, 1};
+Position ghosts[2] = {{10, 5}, {15, 7}};
+char board[HEIGHT][WIDTH] = {
+    "####################",
+    "#........#.........#",
+    "#.####.#.###.#.####.#",
+    "#.#...........#....#",
+    "#.#.#####.#.#####.#",
+    "#.#.....#.#...#...#",
+    "#.#####.#.###.#.###",
+    "#.................#",
+    "####################"
+};
+
+// Fonction pour dessiner le tableau
 void drawBoard() {
-    system("clear");
+    clearScreen();
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            printf("%c", board[y][x]);
+            if (x == pacman.x && y == pacman.y) {
+                printf("%c", PACMAN);  // Pac-Man
+            } else {
+                int isGhost = 0;
+                for (int g = 0; g < 2; g++) {
+                    if (ghosts[g].x == x && ghosts[g].y == y) {
+                        printf("%c", GHOST);  // Fantômes
+                        isGhost = 1;
+                        break;
+                    }
+                }
+                if (!isGhost) {
+                    printf("%c", board[y][x]);  // Le reste du plateau (murs, pastilles)
+                }
+            }
         }
         printf("\n");
     }
-    printf("Score P1: %d | Score P2: %d\n", score1, score2);
 }
 
-int kbhit() {
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if (ch != EOF) {
-        ungetc(ch, stdin);
-        return 1;
-    }
-
-    return 0;
+// Fonction pour jouer un son multiplateforme
+void playSound(int frequency, int duration) {
+    #ifdef _WIN32
+        Beep(frequency, duration); // Son sous Windows
+    #else
+        printf("\a");  // Beep sous Unix
+    #endif
 }
 
-void movePlayer(Position *player, char direction) {
-    int newX = player->x, newY = player->y;
-
-    switch (direction) {
-        case 'z': newY--; break;
-        case 'q': newX--; break;
-        case 's': newY++; break;
-        case 'd': newX++; break;
-        case 'o': newY--; break;
-        case 'k': newX--; break;
-        case 'l': newY++; break;
-        case 'm': newX++; break;
+// Exemple d'utilisation
+void checkCollisions() {
+    // Collision avec un fantôme
+    for (int g = 0; g < 2; g++) {
+        if (pacman.x == ghosts[g].x && pacman.y == ghosts[g].y) {
+            playSound(200, 300);  // Son grave pour la collision avec un fantôme
+            printf("Pac-Man a été attrapé !\n");
+            gameOver = 1;
+        }
     }
+    // Collecte d'une pastille
+    if (board[pacman.y][pacman.x] == PELLET) {
+        playSound(1000, 100);  // Son aigu pour la collecte d'une pastille
+        board[pacman.y][pacman.x] = ' ';  // La pastille disparaît
+    }
+}
 
-    if (board[newY][newX] != WALL) {
-        if (board[newY][newX] == FOOD) {
-            if (*player == player1) {
-                score1++;
-            } else {
-                score2++;
+void checkLevelCompletion() {
+    int pelletsRemaining = 0;
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            if (board[y][x] == PELLET) {
+                pelletsRemaining++;
             }
-            foodCount--;
+        }
+    }
+    if (pelletsRemaining == 0) {
+        printf("Niveau terminé !\n");
+        level++;
+        loadNextLevel();  // Charger le prochain niveau
+    }
+}
+
+void loadNextLevel() {
+    // Charger une nouvelle carte pour le niveau suivant
+    char newBoard[HEIGHT][WIDTH] = {
+        "####################",
+        "#......#....#......#",
+        "#.####.#.##.#.####.#",
+        "#.#............#...#",
+        "#.#.######.#####.#.#",
+        "#.#.......#......#.#",
+        "#.######.#.#####.#.#",
+        "#........#........#",
+        "####################"
+    };
+    // Copier la nouvelle carte dans le tableau global
+    memcpy(board, newBoard, sizeof(newBoard));
+    // Réinitialiser Pac-Man et les fantômes
+    pacman.x = 1; pacman.y = 1;
+    ghosts[0].x = 10; ghosts[0].y = 5;
+    ghosts[1].x = 15; ghosts[1].y = 7;
+}
+
+void moveGhosts() {
+    for (int g = 0; g < ghostCount; g++) {
+        if (ghosts[g].x < pacman.x) {
+            ghosts[g].x++;
+        } else if (ghosts[g].x > pacman.x) {
+            ghosts[g].x--;
         }
 
-        board[player->y][player->x] = EMPTY;
-        player->x = newX;
-        player->y = newY;
-        board[player->y][player->x] = (*player == player1) ? PLAYER1 : PLAYER2;
+        if (ghosts[g].y < pacman.y) {
+            ghosts[g].y++;
+        } else if (ghosts[g].y > pacman.y) {
+            ghosts[g].y--;
+        }
+
+        // Gestion des collisions fantôme-mur
+        if (board[ghosts[g].y][ghosts[g].x] == WALL) {
+            // Si le fantôme frappe un mur, il change de direction
+            ghosts[g].x = (rand() % WIDTH);
+            ghosts[g].y = (rand() % HEIGHT);
+        }
     }
+}
+
+void generateRandomLevel() {
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            int randomTile = rand() % 5;
+            if (randomTile == 0) {
+                board[y][x] = WALL;
+            } else {
+                board[y][x] = PELLET;
+            }
+        }
+    }
+    // Créer une zone vide autour de la position initiale de Pac-Man
+    pacman.x = 1;
+    pacman.y = 1;
+    board[pacman.y][pacman.x] = EMPTY;
+}
+
+void moveSmartGhost(Ghost *ghost) {
+    // Déplacement vers Pac-Man mais éviter les collisions avec d'autres fantômes
+    if (abs(ghost->x - pacman.x) > abs(ghost->y - pacman.y)) {
+        ghost->x += (ghost->x < pacman.x) ? 1 : -1;
+    } else {
+        ghost->y += (ghost->y < pacman.y) ? 1 : -1;
+    }
+    // Si le fantôme frappe un autre fantôme, changer de direction
+    for (int g = 0; g < ghostCount; g++) {
+        if (&ghosts[g] != ghost && ghosts[g].x == ghost->x && ghosts[g].y == ghost->y) {
+            ghost->x = (rand() % WIDTH);
+            ghost->y = (rand() % HEIGHT);
+        }
+    }
+}
+
+void updateHighScores(int score) {
+    FILE *file = fopen("highscores.txt", "a");
+    fprintf(file, "Score: %d\n", score);
+    fclose(file);
+}
+
+void displayHighScores() {
+    FILE *file = fopen("highscores.txt", "r");
+    char line[100];
+    printf("Meilleurs Scores:\n");
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+    fclose(file);
 }
 
 int main() {
-    srand(time(0));
-    initBoard();
-    
-    while (foodCount > 0) {
+    initGame();
+    displayHighScores();  // Affiche les scores avant de commencer
+
+    while (!gameOver) {
         drawBoard();
-
-        if (kbhit()) {
-            char key = getchar();
-            movePlayer(&player1, key);
-            movePlayer(&player2, key);
-        }
-
-        usleep(100000); // Delay to control speed
+        processInput();
+        moveGhosts();  // Déplacer les fantômes de manière intelligente
+        checkCollisions();
+        checkLevelCompletion();
+        wait(200);
     }
 
-    printf("Game Over! Final Scores - P1: %d | P2: %d\n", score1, score2);
+    updateHighScores(score);  // Mise à jour du tableau de scores après la fin du jeu
+    printf("Game Over! Score: %d\n", score);
     return 0;
 }
